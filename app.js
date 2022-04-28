450033a46fd228b216363ef6b08c343d53fe62f9
Stephen Martinez <104386666+SdMartinez13@users.noreply.github.com>
2022-04-27 23:50:43 -0700

    add js to html

---
+++ app.js
@@ -0,0 +1,19 @@
+// 'use strict';
+
+// console.log('this is working')
+
+// let userName = prompt("What is your name?");
+
+// console.log(userName);
+
+
+// // alert("Welcome, " + userName)
+
+// let userInput = prompt("Do you LOVE JavaScript?")
+// if (userInput === 'yes') {
+//     alert('Sweet!!!!!')
+// } else if (userInput === 'no') {
+//     alert('AWWWWWWWWW!!!!')
+// } else {
+//     alert('thats not the answer we were looking for')
+// }
