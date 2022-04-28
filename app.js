77818236d2b46642ddc898b37ac69269f5e50541
Stephen Martinez <104386666+SdMartinez13@users.noreply.github.com>
2022-04-27 23:53:48 -0700

    uncomment comment out js

--- app.js
+++ app.js
@@ -1,19 +1,19 @@
-// 'use strict';
+'use strict';
 
-// console.log('this is working')
+console.log('this is working')
 
-// let userName = prompt("What is your name?");
+let userName = prompt("What is your name?");
 
-// console.log(userName);
+console.log(userName);
 
 
-// // alert("Welcome, " + userName)
+// alert("Welcome, " + userName)
 
-// let userInput = prompt("Do you LOVE JavaScript?")
-// if (userInput === 'yes') {
-//     alert('Sweet!!!!!')
-// } else if (userInput === 'no') {
-//     alert('AWWWWWWWWW!!!!')
-// } else {
-//     alert('thats not the answer we were looking for')
-// }
+let userInput = prompt("Do you LOVE JavaScript?")
+if (userInput === 'yes') {
+    alert('Sweet!!!!!')
+} else if (userInput === 'no') {
+    alert('AWWWWWWWWW!!!!')
+} else {
+    alert('thats not the answer we were looking for')
+}
